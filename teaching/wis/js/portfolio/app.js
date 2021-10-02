82888fecc7a9ffc116481645aaea83dc30efe0f9
Doelia <kyxalia@gmail.com>
2021-10-02 15:26:44 +0200

    :sparkles:

--- teaching/wis/js/portfolio/app.js
+++ teaching/wis/js/portfolio/app.js
@@ -1,41 +1,27 @@
-// Galerie photo
-let images = document.querySelectorAll(".miniature img");
-let main = document.getElementById('main-image');
-
-
-document.body.addEventListener('mousemove', function(event) {
-    document.getElementById('popup').style.left = event.clientX + 'px';
-    document.getElementById('popup').style.top = (event.clientY + 15) + 'px';
-});
-
-for (let i = 0; i < images.length; i++) {
-    images[i].addEventListener('click', function() {
-        let src = images[i].getAttribute('src')
-        main.setAttribute('src', src);
-    })
-    images[i].addEventListener('mouseenter', function() {
-        let text = images[i].getAttribute('alt');
-        document.getElementById("popup").innerHTML = text;
-        document.getElementById("popup").style.display = 'block';
-    })
-    images[i].addEventListener('mouseout', function() {
-        document.getElementById("popup").style.display = 'none';
-    })
-}
-
-// Changement de page
+//  Exercice 1 : Dynamiser le menu
 function changerPage(page) {
-    let pages = document.getElementsByClassName('page');
-    for (let i = 0; i < pages.length; i++) {
-        if (pages[i].id === page) {
-            pages[i].style.display = 'block';
-        } else {
-            pages[i].style.display = 'none';
-        }
+
+    console.log(page);
+
+    if (page === 'accueil') {
+        document.getElementById('accueil').style.display = 'block';
+        document.getElementById('photos').style.display = 'none';
+        document.getElementById('contact').style.display = 'none';
+    }
+    if (page === 'photos') {
+        document.getElementById('accueil').style.display = 'none';
+        document.getElementById('photos').style.display = 'block';
+        document.getElementById('contact').style.display = 'none';
+    }
+    if (page === 'contact') {
+        document.getElementById('accueil').style.display = 'none';
+        document.getElementById('photos').style.display = 'none';
+        document.getElementById('contact').style.display = 'block';
     }
 }
 
-// Compteur de caractères
+// Exercice 2 : Compteur de caractère
+
 function compterCaratrere() {
     let nb = document.getElementById('message').value.length;
     document.getElementById('compteur_caracteres').innerHTML = nb;
@@ -48,3 +34,35 @@ function verifierFormulaire(e) {
         return false;
     }
 }
+
+// Exercice 3 : Galerie photo :
+
+let images = document.querySelectorAll(".miniature img");
+let main = document.getElementById('main-image');
+
+for (let i = 0; i < images.length; i++) {
+    images[i].addEventListener('click', function() {
+        let src = images[i].getAttribute('src')
+        main.setAttribute('src', src);
+    })
+}
+
+
+// Exercice 4 : Tooltip
+
+document.body.addEventListener('mousemove', function(event) {
+    document.getElementById('popup').style.left = event.clientX + 'px';
+    document.getElementById('popup').style.top = (event.clientY + 15) + 'px';
+});
+
+for (let i = 0; i < images.length; i++) {
+    images[i].addEventListener('mouseenter', function() {
+        let text = images[i].getAttribute('alt');
+        document.getElementById("popup").innerHTML = text;
+        document.getElementById("popup").style.display = 'block';
+    })
+    images[i].addEventListener('mouseout', function() {
+        document.getElementById("popup").style.display = 'none';
+    })
+}
+
